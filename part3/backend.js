a2ae3c8da48a02f2ff6c8c984ec5e87bdf5e1ab7
DoublEffe <fabiofagiani9@gmail.com>
2023-02-13 19:36:10 +0100

    3.3 done

--- part3/backend.js
+++ part3/backend.js
@@ -1,9 +1,54 @@
+const { response } = require('express')
 const express = require('express')
 
 const app = express()
 
-app.get('/',(request, response) => {
-    response.send('<h1>Hello World!</h1>')
+const persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons',(request, response) => {
+    response.json(persons)
   })
 
-const PORT = 3000
+app.get('/api/persons/:id',(request,response)=>{
+  let id=Number(request.params.id)
+  let person=persons.find(person=>person.id===id)
+  if(person){
+    response.json(person)
+    }
+    else{
+      response.status(404).end()
+    }
+})  
+
+app.get('/info',(request,response) =>{
+  response.send(`<div>
+                  <p>Phonebook has info for ${persons.length} people</p>
+                  <p>${Date()}</p>
+                </div>`)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
