13cbb90ff7db96bf4c44201e63d2dc540dd04029
DoublEffe <fabiofagiani9@gmail.com>
2023-03-03 20:44:13 +0100

    4.8 done

--- part4/test/blogs_api.test.js
+++ part4/test/blogs_api.test.js
@@ -1,15 +1,42 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../bloglist')
+const Blogs = require('../models/blogs')
 
 const api = supertest(app)
+const initialBlogs = [
+  {
+    title: 'prova',
+    author: 'fabio',
+    url: 'http://prova',
+    likes: 2
+  },
+  {
+    title: 'prova1',
+    author: 'fabio1',
+    url: 'http://prova1',
+    likes: 4
+  }
+]
+beforeEach(async () => {
+  await Blogs.deleteMany({})
+  const blogInDb = initialBlogs.map(blog => new Blogs(blog))
+  const promise = blogInDb.map(blog => blog.save())
+  await Promise.all(promise)
+})
 
-test.only('blogs returned as json',async() => {
+test('blogs returned as json',async() => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type',/application\/json/)
+
 },100000)
+
+test('return all blogs',async () => {
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(initialBlogs.length)
+})
 
 afterAll(async () => {
   await mongoose.connection.close()
