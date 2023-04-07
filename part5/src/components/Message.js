525d40757a50acf1db5cb9bd50159d00cc3183df
Fabio Fagiani <49871878+DoublEffe@users.noreply.github.com>
2023-04-07 09:54:44 +0200

    Delete part5/src directory

--- part5/src/components/Message.js
+++
@@ -1,9 +0,0 @@
-const Message = ({ success,error }) => {
-  return(
-    <div>
-      { success !==null && <p className="message" >{ success }</p> }
-      { error !==null && <p className="messagerror" >{ error }</p> }
-    </div>
-  )
-}
-export default Message
